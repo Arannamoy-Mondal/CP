e4b8b069e36c1bcec9aea7c560edad4977ead282
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2024-10-22 13:57:04 +0600

    Modified

--- Problems/test.cpp
+++ Problems/test.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
 #define int long long
 #define nl "\n"
 #define blk " "
@@ -11,8 +13,22 @@
 #define debug(a)
 #define sep(a)
 #endif
-const int N=1e7+5;
+const int N=1e9+5;
+//using namespace __gnu_pbds;
 using namespace std;
+//template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+bitset<N>isprime;
+vector<int>prime;
+void sieve(){
+    for(int i=3;i<N;i+=2)isprime[i]=1;
+    isprime[2]=1;
+    prime.pb(2);
+    for(int i=3;i*i <=N;i+=2){
+        if(isprime[i])for(int j=i*i;j<N;j+=i)isprime[j]=0;
+    }
+    for(int i=3;i<N;i+=2)if(isprime[i])prime.pb(i);
+    for(int i=0;i<100;i++)cout<<prime[i]<<nl;
+}
 int power(int x,int y)
 {
     int res=1;
@@ -32,17 +48,24 @@ void solve();
 int32_t main()
 {
  ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+#endif
+auto cl=clock();
  int ts=1;
+ sieve();
  //cin>>ts;
- while(ts--)
+ rep(Testcase_,1,ts+1)
  {
+    //debug(Testcase_)
     solve();
  }
+auto Time=1.00*(clock()-cl)/CLOCKS_PER_SEC;
+debug(Time)
 }
 void solve()
 {
-  string s;
-  cin>>s;
-  rep(i,0,s.size())s[i]=toupper(s[i]);
-  cout<<s;
+ 
 }
+//69.93s with pre compiler
+//68 without pre compiler
